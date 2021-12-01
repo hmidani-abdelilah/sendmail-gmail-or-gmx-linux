6c636a5226c8ad305e1749bc635699f01c71d7f7
Hmidani Abdelilah <hmidaniabdelilah92@gmail.com>
2021-12-01 15:15:46 +0100

    resolve nn

--- send-email.sh
+++ send-email.sh
@@ -89,25 +89,7 @@ case $CHOICE in
             configuration
             ;;
         4)
-            echo "Check your config:"
-                while :
-                    do
-                    echo
-                    echo -e '\033[1;33m'"Enter [V/v] to view [m/M] to return Menu: " '\033[0m' 
-                    read CAR
-                    case $CAR in
-                        v|V)
-                            cat ${configfile} | ccze -A
-                            ;;
-                        M|m)
-                            sudo ./$0
-                            break
-                            ;;
-                        *)
-                            echo -e '\033[0;31m' "Sorry, I don't understand" '\033[0m'
-                            ;;
-                    esac
-                done
+            Check 
             ;;           
         5)
             echo "Edit your config"
