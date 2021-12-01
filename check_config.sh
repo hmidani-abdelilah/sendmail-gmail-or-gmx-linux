82d3953f789fe864b9fda912f0572b658886ab0d
Hmidani Abdelilah <hmidaniabdelilah92@gmail.com>
2021-12-01 15:15:21 +0100

    resolve n

---
+++ check_config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+Check () {
+    echo "Check your config:"
+    while :
+        do
+        echo
+        echo -e '\033[1;33m'"Enter [V/v] to view [m/M] to return Menu: " '\033[0m' 
+        read CAR
+        case $CAR in
+            v|V)
+                cat ${configfile} | ccze -A
+                ;;
+            M|m)
+                sudo ./$0
+                break
+                ;;
+            *)
+                echo -e '\033[0;31m' "Sorry, I don't understand" '\033[0m'
+                ;;
+        esac
+    done
+}
