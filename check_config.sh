238d3482b10836e8c10e36c36ec92c53e70b621a
Hmidani Abdelilah <hmidaniabdelilah92@gmail.com>
2021-12-01 15:38:45 +0100

    resolve p

--- check_config.sh
+++ check_config.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
 Check () {
-    configfile=/etc/ssmtp/ssmtp.conf
-    echo "Check your config:"
-    while :
-        do
-        echo
-        echo -e '\033[1;33m'"Enter [V/v] to view [m/M] to return Menu: " '\033[0m' 
-        read CAR
-        case $CAR in
-            v|V)
-                cat ${configfile} | ccze -A
-                ;;
-            M|m)
-                sudo ./$0
-                break
-                ;;
-            *)
-                echo -e '\033[0;31m' "Sorry, I don't understand" '\033[0m'
-                ;;
-        esac
-    done
+configfile=/etc/ssmtp/ssmtp.conf
+echo "Check your config:" 
+cat ${configfile} | ccze -A
+echo
+echo
+echo -e '\033[1;33m'"Press any key to return Menu: " '\033[0m' 
+read -rsn1
+sudo ./$0       
 }
