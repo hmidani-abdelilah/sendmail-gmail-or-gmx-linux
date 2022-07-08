153e960eef9a87c47e795025b02e73a3f5193e7e
hmidani abdelilah <hmidaniabdelilah92@gmail.com>
2022-07-08 11:01:24 +0100

    add color

--- pack/links.sh
+++ pack/links.sh
@@ -6,7 +6,8 @@ echo -e '\033[0;34m'"https://myaccount.google.com/lesssecureapps" '\033[0m'
 echo 
 echo -e '\033[0;34m'"https://accounts.google.com/DisplayUnlockCaptcha" '\033[0m' 
 echo
-echo -e '\033[0;34m' "link below to new config 2022" '\033[0m'
+echo -e '\033[0;36m'"link below to new config 2022" '\033[0;31m'
+echo
 echo -e '\033[0;34m'"https://myaccount.google.com/u/1/apppasswords" '\033[0m'
 echo
 echo -e '\033[1;33m'"Press any key to return Menu: " '\033[0m' 
