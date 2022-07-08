8bd900c3fad9cf73b8c0b88e1dd3bb4a03b3cb4a
hmidani abdelilah <hmidaniabdelilah92@gmail.com>
2022-07-08 10:58:12 +0100

    add link 2022 config gmail

--- pack/links.sh
+++ pack/links.sh
@@ -6,6 +6,9 @@ echo -e '\033[0;34m'"https://myaccount.google.com/lesssecureapps" '\033[0m'
 echo 
 echo -e '\033[0;34m'"https://accounts.google.com/DisplayUnlockCaptcha" '\033[0m' 
 echo
+echo -e '\033[0;34m' "link below to new config 2022" '\033[0m'
+echo -e '\033[0;34m'"https://myaccount.google.com/u/1/apppasswords" '\033[0m'
+echo
 echo -e '\033[1;33m'"Press any key to return Menu: " '\033[0m' 
 read -rsn1 
 sudo ./$0
